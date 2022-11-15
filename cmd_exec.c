f57ca926d7d58158e3b09cf50a90c51667839d87
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2022-11-15 23:54:27 +0100

    Update cmd_exec.c

--- cmd_exec.c
+++ cmd_exec.c
@@ -2,8 +2,8 @@
 
 /**
  * is_cdir - checks ":" if is in the current directory.
- * @path: type char pointer char.
- * @i: type int pointer of index.
+ * @path: type char pointer char
+ * @i: type int pointer of index
  * Return: 1 if the path is searchable in the cd, 0 otherwise.
  */
 int is_cdir(char *path, int *i)

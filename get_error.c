df32ad6aabc8435a90ef9ac4024dc302abb1df04
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2022-11-16 00:03:45 +0100

    Update get_error.c

--- get_error.c
+++ get_error.c
@@ -4,7 +4,7 @@
  * get_error - calls the error according the builtin, syntax or permission
  * @datash: data structure that contains arguments
  * @eval: error value
- * Return: error
+ * Return: error.
  */
 int get_error(data_shell *datash, int eval)
 {

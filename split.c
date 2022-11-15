cb4bcfb2abad0282788d91161eb6906f9cee7afb
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2022-11-16 00:11:03 +0100

    Update split.c

--- split.c
+++ split.c
@@ -3,9 +3,9 @@
 /**
  * swap_char - swaps | and & for non-printed chars
  *
- * @input: input string
- * @bool: type of swap
- * Return: swapped string
+ * @input: input string.
+ * @bool: type of swap.
+ * Return: swapped string.
  */
 char *swap_char(char *input, int bool)
 {
